c9ae445d681aacf224d9b37f2864b254cefa5507
mdalp <marius@alpmann.de>
2024-06-03 23:18:18 +0200

    Remove tests, update project name in conf

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EchoServer.MixProject do
 
   def project do
     [
-      app: :echo_server,
+      app: :therapy_server,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
@@ -21,9 +21,6 @@ defmodule EchoServer.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
